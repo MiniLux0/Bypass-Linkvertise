301b1703046be55acb56378ed7337a3e3b85f3eb
Jess-Lux <jrvv972006@gmail.com>
2021-11-12 10:22:56 -0500

    “Bypass”

---
+++ js/main.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.nav', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px'
+});
+
+sr.reveal('.typed', {
+  duration: 4000,
+});
+
+sr.reveal(".hero__title", {
+  duration: 4000,
+});
+
+sr.reveal(".logic", {
+  duration: 4000,
+});
